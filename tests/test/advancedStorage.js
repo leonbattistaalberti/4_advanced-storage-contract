c0b37e44f19b985f9e1110f5dd7a9cefc019a464
thelonehegelian <thelonehegelian@gmail.com>
2020-08-18 05:34:16 +0100

    Refactor tests with before statement

--- tests/test/advancedStorage.js
+++ tests/test/advancedStorage.js
@@ -1,28 +1,28 @@
 const AdvancedStorage = artifacts.require("AdvancedStorage");
 
 contract("AdvancedStorage", () => {
-	it("Adds an item to the array", async () => {
+	let advancedStorage = null;
+	before(async () => {
 		const advancedStorage = await AdvancedStorage.deployed();
+	});
+	it("Adds an item to the array", async () => {
 		await advancedStorage.add(10);
 		let result = await advancedStorage.ids(0);
 		assert(result.toNumber() === 10);
 	});
 
 	it("Gets specified item from the array", async () => {
-		const advancedStorage = await AdvancedStorage.deployed();
 		await advancedStorage.add(20);
 		let result = await advancedStorage.get(1);
 		assert(result.toNumber() === 20);
 	});
 	it("Get the entire array", async () => {
-		const advancedStorage = await AdvancedStorage.deployed();
 		await advancedStorage.add(30);
 		let rawIds = await advancedStorage.getAll();
 		let ids = rawIds.map((id) => id.toNumber());
 		assert.deepEqual(ids, [10, 20, 30]);
 	});
 	it("Returns the correct length of the array", async () => {
-		const advancedStorage = await AdvancedStorage.deployed();
 		let result = await advancedStorage.length();
 		assert(result.toNumber() === 3);
 	});
