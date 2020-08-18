d7f1ca072dd5b71edba3619ec1bc339460799437
thelonehegelian <thelonehegelian@gmail.com>
2020-08-18 09:28:05 +0100

    Add DOM event listener

--- frontend/client/index.js
+++ frontend/client/index.js
@@ -1,1 +1,29 @@
 import Web3 from "web3";
+import AdvancedStorage from "../build/contracts/AdvancedStorage.json";
+
+const web3;
+const advancedStorage
+
+// initialize web3 instance
+const initWeb3 = () => {}
+
+// initialize smart contract instance
+const initContract = () => {}
+
+// initialize app
+const initApp = () => {}
+
+// load DOM
+
+document.addEventListener("DOMContentLoaded", () => {
+    // initialize the instance of Web3
+    initWeb3().then((_web3) => {
+        web3 = _web3;
+        // initialize the smart contract 
+        advancedStorage = initContract();
+        // initialize app
+        initApp();
+
+        catch (e) { console.log(e.message); }
+    })
+})
