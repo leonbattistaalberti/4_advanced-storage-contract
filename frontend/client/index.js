f0217575e0a76e62ad1551fadc3b78ee99dc0102
thelonehegelian <thelonehegelian@gmail.com>
2020-08-19 00:54:20 +0100

    Add initApp function

--- frontend/client/index.js
+++ frontend/client/index.js
@@ -12,20 +12,21 @@ let advancedStorage;
 // initialize web3 instance
 const initWeb3 = () => {
 	return new Promise((resolve, reject) => {
-		// 1. If new Metamask was loaded
+		// 1. If new Metamask was loaded in the browser
 		if (typeof window.ethereum !== "undefined") {
 			window.ethereum
-				.enable()
+				.enable() // returns a promise of array of user accounts
 				.then(() => {
 					resolve(new Web3(window.ethereum));
 				})
 				.catch((e) => {
 					reject(e);
 				});
+			return;
 		}
-		return;
+
 		// 2. If new Metamask failed and old Metamask was loaded
-		if (typeof window.web3 === "undefined") {
+		if (typeof window.web3 !== "undefined") {
 			return resolve(new Web3(window.web3.currentProvider));
 		}
 		// 3. If new and old Metamask failed, then load Ganache
@@ -36,6 +37,7 @@ const initWeb3 = () => {
 // initialize smart contract instance
 const initContract = () => {
 	let deploymentKey = Object.keys(AdvancedStorage.networks)[0];
+	// initite smart contract instance
 	return new Web3.eth.Contract(
 		AdvancedStorage.abi,
 		AdvancedStorage.networks[deploymentKey].address
@@ -43,11 +45,46 @@ const initContract = () => {
 };
 
 // initialize app
-const initApp = () => {};
+const initApp = () => {
+	// 1. load accounts
+	let accounts = [];
+	// get DOM elements
+	let $addData = document.getElementById("addData");
+	let $data = document.getElementById("data");
+
+	/**
+	 * web3.eth.accounts
+	 * Promise returns Array
+	 *
+	 */
+
+	web3.eth
+		.getAccounts()
+		.then((_accounts) => {
+			accounts = _accounts;
+			// get all accounts
+			return advancedStorage.methods.getAll().call();
+		})
+		// 2. display the data on the screen
+		.then((result) => ($data.innerHTML = result.join(", ")));
+
+	// 3. add data to ids array
+	$addData.addEventListener("submit", (evt) => {
+		evt.preventDefault();
+		let data = evt.target.elements[0].value;
+		advancedStorage.methods
+			.add(data)
+			.send({ from: accounts[0] })
+			.then(() => {
+				return advancedStorage.methods.getAll().call();
+			})
+			.then((result) => ($data.innerHTML = result.join(", ")));
+	});
+};
 
 // load DOM
 document.addEventListener("DOMContentLoaded", () => {
-	// initialize the instance of Web3
+	// initialize an instance of Web3
 	initWeb3()
 		.then((_web3) => {
 			web3 = _web3;
